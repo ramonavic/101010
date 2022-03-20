bdbad469b5d6d78a23aa02ac58c5ecb6c07b914c
ramon <ramon@dedact.nl>
2022-03-20 15:08:26 -0500

    add basic card style

--- components/PlaylistCard.vue
+++ components/PlaylistCard.vue
@@ -1,56 +1,82 @@
 <template>
     <div class="container card">
-        <div class="card-image">
-            <figure class="image is-4by3">
-                <img :src="playlist.image" alt="Spotify playlist">
-            </figure>
+        <div class="top">
+            <div class="card-image" :style="getBackground()"></div>
             <div class="title over-image"> {{playlist.name}} </div>
-        </div>
-        <div class="card-content">
-            <div class="media">
-                <!-- <div class="media-left">
-                    <figure class="image is-48x48">
-                    <img :src="playlist.image" alt="Placeholder image">
-                    </figure> -->
-                <!-- </div> -->
-                <div class="media-content">
-                    <!-- <p class="title is-4">{{playlist.name}}</p> -->
-                    <!-- Can contain user who has posted later -->
-                    <!-- <p class="subtitle is-6"></p> --> 
+            <div class="card-content">
+                <div class="content">
+                {{playlist.description}}
+
+                <!-- TODO make tag component -->
+                <a href="#">#css</a> <a href="#">#responsive</a>
+                <br>
+                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
                 </div>
             </div>
-
-            <div class="content">
-            {{playlist.description}}
-
-            <!-- TODO make tag component -->
-            <a href="#">#css</a> <a href="#">#responsive</a>
-            <br>
-            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-            </div>
         </div>
-        <iframe :src="getEmbedLink()" width="100%" height="250" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-           <footer class="card-footer">
+        <iframe :src="getEmbedLink()" class="playlist" width="100%" height="250" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+        <footer class="card-footer">
 
-               <!-- TODO hier moet een Spotify icoon komen -->
-                <a href="#" class="card-footer-item">Open in Spotify</a>
-                <!-- TODO hier moet een hartje icoon komen -->
-                <a href="#" class="card-footer-item">Add Playlist</a>
-            </footer>
+            <!-- TODO hier moet een Spotify icoon komen -->
+            <a href="#" class="card-footer-item">Open in Spotify</a>
+            <!-- TODO hier moet een hartje icoon komen -->
+            <a href="#" class="card-footer-item">Add Playlist</a>
+        </footer>
     </div>
- 
 </template>
 
 <style scoped lang="scss">
 .card {
     margin-bottom: 2rem;
     max-width: 400px !important;
-    background-color: #191414;
+    background-color: #0a0a0a;
+    border: 1px solid $grey-dark;
+    box-shadow: 14px 14px 22px -6px #094646;
 
     .over-image {
         position: absolute;
+        top: 2rem;
+        left: 2rem;
+        z-index: 2;
+    }
+
+    .top {
+        position: relative;
+        margin: 1rem;
+        // background-color: #0a0a0a;
+    }
+
+    .card-image {
+        z-index: 1;
+        margin-bottom: 1rem;
+        height: 15em;
+        width: 100%;
+        background-repeat: no-repeat;
+        background-position: top;
+        background-size: cover;
+        filter: blur(1px);
+    }
+
+    .card-content {
+        position: absolute;
         bottom: 1rem;
         left: 2rem;
+        z-index: 2;
+        padding: 0;
+        color: $white;
+
+        a {
+            color: $white;
+
+            &:hover {
+                color: $grey-lighter;
+            }
+        }
+    }
+
+    .playlist {
+        padding: 0 1rem;
+        background-color: #0a0a0a;
     }
 }
 </style>
